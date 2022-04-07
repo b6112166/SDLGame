3cd43f379a358e6e4db6641e6e4608a5a988bec2
b6112166 <959321146@qq.com>
2022-04-07 15:20:39 -0700

    sword

--- Game00/Sword.h
+++ Game00/Sword.h
@@ -1,17 +1,17 @@
 #pragma once
-#include "Entity.h"
+#include "Player.h"
 #include "Direction.h"
 class Sword :
     public Entity
 {
 public:
-    Sword(Entity * owner) :Entity(owner->getPosX(), owner->getPosY(), 13, 37) {};
+    Sword(Player* owner, SDL_Texture* texture) :Entity(owner->getPosX(), owner->getPosY(), 13, 37) { swordTexture = texture; };
     void update();
 
 
 private:
     
-
+    SDL_Texture * swordTexture ;
 
 
 };
